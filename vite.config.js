7822f6141b90368ada6eab78b6c112229358e814
nturuciu <karubiu2017@gmail.com>
2023-10-07 23:35:09 +0300

    first commit

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+// //JSX
+
+const FunctionalComponent = ()=>{
+
+  return (
+    //JSX codes returned here
+    <>
+      <h1>Hello World</h1>
+    </>
+  )
+}
